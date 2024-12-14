d6d41709757c0fb0363c7ba3ac74de118b97ec06
Rishav Das <rishavdas27602@gmail.com>
2024-12-14 21:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function firstWord(s) {
+//function firstWord(s) {
   // your code here
+//}
+function firstWord(str) {
+    // Remove leading spaces
+    str = str.trimStart();
+    
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Return the substring up to the first space
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
